fc54e47bd9ba40dc1fa37c3e590c265a9c9dd734
Aaron Nies <arocnies@gmail.com>
2019-04-03 22:59:22 -0400

    Added health endpoint

--- src/main/kotlin/io/ossim/omar/apps/volume/cleanup/app/App.kt
+++ src/main/kotlin/io/ossim/omar/apps/volume/cleanup/app/App.kt
@@ -1,12 +1,21 @@
 package io.ossim.omar.apps.volume.cleanup.app
 
+import io.ktor.application.call
 import io.ktor.client.engine.apache.Apache
+import io.ktor.http.ContentType
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
 import io.ossim.omar.apps.volume.cleanup.log
 import io.ossim.omar.apps.volume.cleanup.raster.RasterClient
 import io.ossim.omar.apps.volume.cleanup.raster.SizeRestrictedRasterVolume
 import io.ossim.omar.apps.volume.cleanup.raster.database.RasterDatabase
 import kotlinx.coroutines.time.delay
 import java.io.File
+import java.util.*
 
 suspend fun main() {
     val config = Configuration()
@@ -25,6 +34,14 @@ suspend fun main() {
         percentThreshold = config.percentThreshold,
         dryRun = config.dryRun
     )
+
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get("/health") {
+                call.respondText("[${Date()}] I'm alive", ContentType.Text.Plain, HttpStatusCode.OK)
+            }
+        }
+    }.start()
 
     while (true) {
         log("Restricting volume size")
