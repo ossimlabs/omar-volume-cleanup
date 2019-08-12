330345265851e631e060fb367985855bd0087eda
arocnies <arocnies@gmail.com>
2019-08-12 09:36:32 -0400

    Update build.gradle.kts
    Set java opts to an environment variable string

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,13 +39,12 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-val javaOpts = "-server -Xms256m -XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XX:+CMSClassUnloadingEnabled -XX:+UseGCOverheadLimit -Djava.awt.headless=true -XshowSettings:vm -Djava.security.egd=file:/dev/./urandom"
-
 jib {
     container.mainClass = "io.ossim.omar.apps.volume.cleanup.app.AppKt"
     container.volumes = listOf("/data")
-    container.jvmFlags = javaOpts.split(' ')
+    container.jvmFlags = listOf("\$JAVA_OPTS")
     container.environment = mapOf(
+        "JAVA_OPTS" to "" // Used by the O2 apps deployment to configure memory limits
         "CLEANUP_DRYRUN" to "true", // Default to true to avoid accidental deletions
         "CLEANUP_VOLUME" to "/data",
         "CLEANUP_DELAY" to "10m", // Ten minute default
